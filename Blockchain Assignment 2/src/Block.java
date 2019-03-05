e33db9c03bfc736ad6b7f1520daa7136fc99eb37
ABassem16 <45358489+ABassem16@users.noreply.github.com>
2019-03-05 15:39:55 +0200

    Added Project Files

--- Blockchain Assignment 2/src/Block.java
+++ Blockchain Assignment 2/src/Block.java
@@ -1,11 +1,26 @@
-
+import java.sql.Timestamp;
+import java.security.MessageDigest;
 public class Block
 {
 	private int index;
-	private int timestamp;
+	private Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 	private String data;
-	private int hash;
-	private int prevhash;
+	private String hash;
+	private String prevhash;
+	public void sha(String input)
+	{
+	try 
+	{
+		
+	MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	byte[] sha = digest.digest(input.getBytes());
+	System.out.println(sha);
+	}
+	catch(Exception ex)
+	{
+		throw new RuntimeException(ex);
+	}
+	}
 	
 	public int getIndex() {
 		return index;
@@ -13,28 +28,36 @@ public class Block
 	public void setIndex(int index) {
 		this.index = index;
 	}
-	public int getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(int timestamp) {
-		this.timestamp = timestamp;
-	}
 	public String getData() {
 		return data;
 	}
 	public void setData(String data) {
 		this.data = data;
 	}
-	public int getPrevhash() {
+	public String getPrevhash() {
 		return prevhash;
 	}
-	public void setPrevhash(int prevhash) {
+	public void setPrevhash(String prevhash) {
 		this.prevhash = prevhash;
 	}
-	public int getHash() {
+	public String getHash() {
 		return hash;
 	}
-	public void setHash(int hash) {
+	public void setHash(String hash) {
 		this.hash = hash;
+	}
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+	public static void main(String[] args)
+	{
+		Block b=new Block();
+		Timestamp ts=b.getTimestamp();
+		System.out.println(ts);
+		String x="Hello";
+		b.sha(x);
 	}
 }
